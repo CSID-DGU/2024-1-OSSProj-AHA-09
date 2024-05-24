32fd44413a865a5a1edd3d8c876388f97f7e1dbc
gunheee-leee <ish8176@dgu.ac.kr>
2024-05-24 22:16:11 +0900

    지도 투명도 설정, 위도, 경도 값 받아오기

--- myWeb/map/models.py
+++ myWeb/map/models.py
@@ -1,22 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Rice(models.Model):
-    year=models.IntegerField()
-    region=models.TextField()
-    prediction=models.FloatField()
-
-class Barley(models.Model):
-    year=models.IntegerField()
-    region=models.TextField()
-    prediction=models.FloatField()
-
-class Corn(models.Model):
-    year=models.IntegerField()
-    region=models.TextField()
-    prediction=models.FloatField()
-
-class Bean(models.Model):
+class Data(models.Model):
+    crops=models.TextField()
     year=models.IntegerField()
     region=models.TextField()
     prediction=models.FloatField()
