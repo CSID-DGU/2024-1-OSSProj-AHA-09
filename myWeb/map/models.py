a98d19c30f6783e08496e46c6ab7df69698f6b7b
gunheee-leee <ish8176@dgu.ac.kr>
2024-05-24 19:19:48 +0900

    생산량 별 색깔, 테이블구조 구현

--- myWeb/map/models.py
+++ myWeb/map/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Rice(models.Model):
+    year=models.IntegerField()
+    region=models.TextField()
+    prediction=models.FloatField()
+
+class Barley(models.Model):
+    year=models.IntegerField()
+    region=models.TextField()
+    prediction=models.FloatField()
+
+class Corn(models.Model):
+    year=models.IntegerField()
+    region=models.TextField()
+    prediction=models.FloatField()
+
+class Bean(models.Model):
+    year=models.IntegerField()
+    region=models.TextField()
+    prediction=models.FloatField()
